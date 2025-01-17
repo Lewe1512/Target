d50af3a66643216de2d7c22388a50c3a8bb9532d
Lewe1512 <pulsehub@jovhub.xyz>
2025-01-17 19:40:41 +0700

    Update target.lua

--- target.lua
+++ target.lua
@@ -1,5 +1,5 @@
 local gameId = game.PlaceId
     
 if gameId == 6137321701 then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/Bang801/games/refs/heads/main/blair.lua"))()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Lewe1512/game/refs/heads/main/pifqsf.lua"))()
 end 
