6f08b1b2add412a0a2f2b74c362f277016a45977
Lewe1512 <pulsehub@jovhub.xyz>
2025-01-17 19:57:48 +0700

    Update target.lua

--- target.lua
+++ target.lua
@@ -1,5 +1,7 @@
 local gameId = game.PlaceId
     
-if gameId == 6137321701 then
+if gameId == 6137321701 or gameId == 6348640020 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/Lewe1512/game/refs/heads/main/pifqsf.lua"))()
+else
+    game.Players.LocalPlayer:Kick("[Pulse Hub] This game is currently not supported.\nWe are constantly expanding our game library - check back soon for updates!")
 end 
