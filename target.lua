fe1875e0a801bf5925fe5786f8ea67c8a04466a0
Lewe1512 <pulsehub@jovhub.xyz>
2025-01-19 06:35:47 +0700

    Update target.lua

--- target.lua
+++ target.lua
@@ -4,6 +4,8 @@ if gameId == 537413528 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/Lewe1512/game/refs/heads/main/safwq.lua"))()
 elseif gameId == 6137321701 or gameId == 6348640020 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/Lewe1512/game/refs/heads/main/pifqsf.lua"))()
+elseif gameId == 8260276694 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Lewe1512/game/refs/heads/main/hfsvg.lua"))()
 else
     game.Players.LocalPlayer:Kick("[Pulse Hub] This game is currently not supported.\nWe are constantly expanding our game library - check back soon for updates!")
 end 
